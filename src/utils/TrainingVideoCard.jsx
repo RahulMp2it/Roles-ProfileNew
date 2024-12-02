275058403d9cef46a67d8d96f3e5faa7ed14cb40
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-02 18:15:58 +0530

    use prop link and onlick

--- src/utils/TrainingVideoCard.jsx
+++ src/utils/TrainingVideoCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TrainingVideoCard = () => {
+const TrainingVideoCard = ({ link, onClick }) => {
   return (
     <div className='w-[160px] h-[160px] bg-[#D8D8D83D] rounded-xl '>
       <div className='h-[90px] w-[90px] ml-7 mt-4'><img src="vedio.png" alt="Loading...." /></div>
