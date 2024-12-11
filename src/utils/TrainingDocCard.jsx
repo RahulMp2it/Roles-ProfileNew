b8ba585c588eb0a12d036acb18dee747e997f4c9
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-11 18:22:48 +0530

    create new card to handle prop to watch word file

--- src/utils/TrainingDocCard.jsx
+++ src/utils/TrainingDocCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function TrainingDocCard() {
+export default function TrainingDocCard({ name, docPath }) {
   return (
     <div className='w-[160px] h-[160px] bg-[#D8D8D83D] rounded-xl '>
-      <div className='h-[100px] w-[100px] mt-5 ml-5 '><img src="doc.png" alt="Loading...." /></div>
-      <div className="ml-7 mt-1">UI/UX Designer 1</div>
-      {/* <a href="" download>Download</a> */}
+      <Link to={`/docview?docPath=${encodeURIComponent(docPath)}`}>
+
+        <div className='h-[100px] w-[100px] mt-5 ml-5 '>
+          <img src="doc.png" alt="Loading...." /></div>
+        <div className="ml-7 mt-1">{name}</div>
+        {/* <a href="" download>Download</a> */}
+      </Link>
     </div>
   )
 }
