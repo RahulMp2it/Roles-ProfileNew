1a6e977260c2a0231d9dcbe3f291f326a355598c
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-11 18:23:28 +0530

    send link in this component

---
+++ src/utils/TrainingDocView.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+
+function TrainingDocView() {
+
+    const [searchParams] = useSearchParams();
+    const docPath = searchParams.get('docPath'); // Extract docPath from the query string
+
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+          {docPath ? (
+            <iframe
+              src={`http://localhost:8080/uploads/${docPath}`}
+              title="Document Viewer"
+              className="w-full h-[90vh]"
+            />
+          ) : (
+            <p className="text-red-500">No document selected.</p>
+          )}
+        </div>
+      );
+}
+
+export default TrainingDocView
