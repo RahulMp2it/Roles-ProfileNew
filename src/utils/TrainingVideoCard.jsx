f5dcf4e07a8042d6fbd0ef6e6d5dcd607e1b7be6
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-11 18:24:40 +0530

    send prop to training vedio view component

--- src/utils/TrainingVideoCard.jsx
+++ src/utils/TrainingVideoCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const TrainingVideoCard = ({ link, onClick }) => {
+const TrainingVideoCard = ({ link, onClick, name, path1 }) => {
   return (
     <div className='w-[160px] h-[160px] bg-[#D8D8D83D] rounded-xl '>
-      <div className='h-[90px] w-[90px] ml-7 mt-4'><img src="vedio.png" alt="Loading...." /></div>
-      <div className="ml-7 mt-4">UI/UX Designer 1</div>
+      <Link to={`/videoview?path1=${encodeURIComponent(path1)}`}>
+      
+      <div className='h-[90px] w-[90px] ml-7 mt-4'>
+        <img src="vedio.png" alt="Loading...." />
+       </div>
+        <div className="ml-7 mt-4">{name}</div>
       {/* <a href="" download>Download</a> */}
+      </Link>
     </div>
   )
 }
