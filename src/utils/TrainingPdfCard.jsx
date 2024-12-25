1d0e42caa7219565fe22adc8f373264d4423e4e2
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-25 18:16:31 +0530

    use link and also take props

--- src/utils/TrainingPdfCard.jsx
+++ src/utils/TrainingPdfCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function TrainingPdfCard() {
+const TrainingPdfCard = ({ name, pdfPath }) => {
+
   return (
     <div className='w-[160px] h-[160px] bg-[#D8D8D83D] rounded-xl '>
-      <div className='h-[70px] w-[70px] ml-11 mt-8 '><img src="pdf.png" alt="Loading...." /></div>
-      <div className="ml-5 mt-5 ">UI/UX Designer 1</div>
-      {/* <a href="" download>Download</a> */}
+      <Link to={`/pdfview?pdfPath=${encodeURIComponent(pdfPath)}`}>
+
+      <div className='h-[70px] w-[70px] ml-11 mt-8 '>
+        <img src="pdf.png" alt="Loading...." />
+      </div>
+      <div className="ml-5 mt-5 ">{name}</div>
+      
+      </Link>
     </div>
   )
 }
